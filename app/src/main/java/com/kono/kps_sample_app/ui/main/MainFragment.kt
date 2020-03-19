7481fabb3a8339dff342c84a5de32025f1a56c04
YSH <yshuang@thekono.com>
2020-03-19 12:27:33 +0800

    First commit

---
+++ app/src/main/java/com/kono/kps_sample_app/ui/main/MainFragment.kt
@@ -0,0 +1,36 @@
+package com.kono.kps_sample_app.ui.main
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.kono.kps_sample_app.R
+import com.kono.kpssdk.*
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        val v = inflater.inflate(R.layout.main_fragment, container, false)
+        val tv = v.findViewById<TextView>(R.id.message)
+        tv.text = Test().yoyoyo
+
+        return v
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/kono/kps_sample_app/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.kono.kps_sample_app.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
