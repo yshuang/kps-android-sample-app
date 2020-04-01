4857aad88b06e7740feb4c51e5859ad19ad450df
YSH <yshuang@thekono.com>
2020-04-01 17:05:26 +0800

    change to kps first data version

--- app/src/main/java/com/kono/kps_sample_app/MainActivity.kt
+++ app/src/main/java/com/kono/kps_sample_app/MainActivity.kt
@@ -3,11 +3,17 @@ package com.kono.kps_sample_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kono.kps_sample_app.ui.main.MainFragment
+import com.kono.kpssdk.core.KPS
+import com.kono.kpssdk.core.init
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        KPS.init(this, "5dfafdc23682f5322ec92bcb")
+
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
--- app/src/main/java/com/kono/kps_sample_app/ui/main/MainFragment.kt
+++ app/src/main/java/com/kono/kps_sample_app/ui/main/MainFragment.kt
@@ -2,13 +2,15 @@ package com.kono.kps_sample_app.ui.main
 
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import com.kono.kps_sample_app.R
-import com.kono.kpssdk.*
+import com.kono.kpssdk.core.KPS
+import com.kono.kpssdk.core.openKPSContent
 
 class MainFragment : Fragment() {
 
@@ -22,7 +24,34 @@ class MainFragment : Fragment() {
                               savedInstanceState: Bundle?): View {
         val v = inflater.inflate(R.layout.main_fragment, container, false)
         val tv = v.findViewById<TextView>(R.id.message)
-        tv.text = Test().yoyoyo
+        tv.text = "asdfaaa"
+
+        KPS.openKPSContent("5e451b0004012eecf1615436"){ errMsg, folder, children, orderChildrenIncreaseRight ->
+            Log.d("AAA", errMsg ?: "no error");
+            Log.d("AAA", folder?.id ?: "null id");
+            Log.d("AAA", folder?.type ?: "null type");
+            Log.d("AAA", children?.get(0)?.id ?: "null child id");
+            Log.d("AAA", children?.toString() ?: "null children");
+            Log.d("AAA",  orderChildrenIncreaseRight.toString());
+
+            if(folder != null){
+                for(anImage in folder.images){
+                    Log.d("AAA",  anImage.getUri(100));
+                    Log.d("AAA",  "============");
+                    Log.d("AAA",  anImage.getUri(200));
+                    Log.d("AAA",  "============");
+                    Log.d("AAA",  anImage.getUri(300));
+                    Log.d("AAA",  "============");
+                    Log.d("AAA",  anImage.getUri(400));
+                    Log.d("AAA",  "============");
+                    Log.d("AAA",  anImage.getUri(500));
+                    Log.d("AAA",  "============");
+                    Log.d("AAA",  anImage.getUri(600));
+                }
+            }
+
+            Log.d("AAA", folder?.toString() ?: "Empty folder")
+        }
 
         return v
     }
